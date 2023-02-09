65cc959ffb76a61f70394b326214a90911fd377e
19YBeABhyk9tY5z5zGbA3oQLU5MFExpb8o <46433702+c4pt000@users.noreply.github.com>
2023-02-09 14:43:50 -0500

    Update MainNetParams.java

--- core/src/main/java/org/bitcoinj/params/MainNetParams.java
+++ core/src/main/java/org/bitcoinj/params/MainNetParams.java
@@ -38,9 +38,9 @@ public class MainNetParams extends AbstractBitcoinNetParams {
         targetTimespan = TARGET_TIMESPAN;
         maxTarget = Utils.decodeCompactBits(0x1e0ffff0L);
         dumpedPrivateKeyHeader = 158;
-        addressHeader = 60;
+        addressHeader = 25;
         p2shHeader = 22;
-        segwitAddressHrp = "radc";
+        segwitAddressHrp = "bits";
         port = 8333;
         packetMagic = 0xd1d1d1d1L;
         bip32HeaderP2PKHpub = 0x02facafd; //The 4 byte header that serializes in base58 to "dgub".
@@ -51,14 +51,14 @@ public class MainNetParams extends AbstractBitcoinNetParams {
         majorityWindow = MAINNET_MAJORITY_WINDOW;
 
  genesisBlock.setDifficultyTarget(0x1e0ffff0L);
-        genesisBlock.setTime(1622171724L);
-        genesisBlock.setNonce(491158L);
+        genesisBlock.setTime(1675886721L);
+        genesisBlock.setNonce(1195632L);
         subsidyDecreaseBlockCount = 100000;
         spendableCoinbaseDepth = 10;
 
         id = ID_MAINNET;
         String genesisHash = genesisBlock.getHashAsString();
-        checkState(genesisHash.equals("000007ce46e6c59844c34fa7ba5b27c8dac0653a27fcfb7340cc0158849e4afd"),
+        checkState(genesisHash.equals("0000049008321e63472304c0fc8d3937b7679cdb2144f8ebe8b8fb29e4deae94"),
                 genesisHash);
 
         majorityEnforceBlockUpgrade = MAINNET_MAJORITY_ENFORCE_BLOCK_UPGRADE;
@@ -69,14 +69,20 @@ public class MainNetParams extends AbstractBitcoinNetParams {
         // transactions are handled. Duplicated transactions could occur in the case where a coinbase had the same
         // extraNonce and the same outputs but appeared at different heights, and greatly complicated re-org handling.
         // Having these here simplifies block connection logic considerably.
-        checkpoints.put(60000, Sha256Hash.wrap("cf23fe03d921012cfdfc2847bd6ef4a647bbb559d6eb57d6432d7bcb930d720f"));
+        checkpoints.put(600, Sha256Hash.wrap("1c0bfeebb428e46e8a96944f9adf027fe07140f7a011a4500b8db8ffa62bcbe7"));
 
         
         
   // ?? risky business readding dogecoin dns seeds here to attempt experimentation with peer to peer "bloom filter bit" that might be missing from radiocoin dns seed points for android wallet support (in experimentation to match protocol header)
         dnsSeeds = new String[] {
-                "radiopool.me",
-            "46.101.169.238",
+                "multidoge.org",
+            "seed.multidoge.org",
+            "seed01.altcoinbuilders.com",
+            "seed02.altcoinbuilders.com",
+            "seed03.altcoinbuilders.com",
+            "seed04.altcoinbuilders.com",
+            "170.187.167.146",
+            
             };
     }
     private static MainNetParams instance;
